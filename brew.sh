a7679219b1dcac75eb38c6486efd2720ed65d495
Isto Nikula <isto.nikula@iki.fi>
2015-07-01 14:46:11 +0300

    Random updates from the upstream

--- brew.sh
+++ brew.sh
@@ -16,14 +16,14 @@ brew install findutils --default-names
 # Install GNU  `tar`, `g`-prefixed
 brew install gnu-tar
 # Install GNU `sed`, overwriting the build-in `sed`
-brew install gnu-sed  --default-names
+brew install gnu-sed  --with-default-names
 # Install Bash 4
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before running `chsh`.
 brew install bash
 brew install bash-completion
 
 # Install wget with IRI support
-brew install wget --enable-iri
+brew install wget --with-iri
 
 # Install more recent versions of some OS X tools
 brew install vim --override-system-vi
