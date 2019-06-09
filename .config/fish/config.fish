6371cca356d79036fe2a76b1fe994e9c4baff46f
Isto Nikula <isto.nikula@iki.fi>
2019-06-09 09:22:07 +0300

    fish: misc utils

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,20 @@
 
-set -x PATH $PATH ~/bin
+# go
+set -x GOROOT /usr/local/go
+set -x GOPATH ~/go
+
+# ruby
+set -x GEM_HOME ~/gems
+
+# aws vault
+set -x AWS_VAULT_BACKEND secret-service
+
+
+set -x PATH $PATH ~/bin $GOROOT/bin $GOPATH/bin $GEM_HOME/bin
+
 
 alias ccat='pygmentize'
+
 
 # Base16 Shell
 if status --is-interactive
@@ -49,10 +62,10 @@ function n0
 end
 
 function vpn1
-  nmcli --ask con up id Reaktor\ VPN
+  nmcli --ask con up id ReaktorVPN
 end
 function vpn0
-  nmcli --ask con down id Reaktor\ VPN
+  nmcli --ask con down id ReaktorVPN
 end
 
 # display
@@ -61,28 +74,56 @@ function dpi
 end
 
 
-#
-
 set -xg JAVA_HOME /opt/java/jdk1.8
+# set -xg JAVA_HOME /opt/java/jdk1.10
 
 # . ~/Projects/KONE/env/kone_profile.fish
 
-alias ipext="dig +short myip.opendns.com @resolver1.opendns.com"
+alias ipext="dig @resolver1.opendns.com -4 ANY myip.opendns.com +short"
 
 # docker
 function drm
   docker ps -aq --no-trunc | xargs -r docker rm
 end
 function dls
-  docker ps
+  docker ps --format 'table {{.ID}}\t{{.Image}}' $argv
 end
-function dla
-  docker ps -a
+function dlsc
+  dls | grep $argv[1] | awk '{ print $1 }'
+end
+function dlsi
+  docker images $argv
 end
 function drmi
   docker images -q -f "dangling=true" | xargs -r docker rmi
 end
+function djsonlog
+  set container_id = argv[1]
+  set log (docker inspect --format='{{.LogPath}}' $argv[1])
+  sudo tail -n 100 -f $log | jq '.log | fromjson?'
+end
 
+# check disk smart log
+function sl
+  sudo nvme smart-log /dev/nvme0
+end
 
+function e
+  emacsclient -n $argv
+end
 
+function ef
+  e ~/.config/fish/config.fish
+end
 
+function gradle
+  ./gradlew $argv
+end
+
+function lerna
+  ./node_modules/.bin/lerna $argv
+end
+
+function xx
+  xmodmap ~/.Xmodmap
+end
